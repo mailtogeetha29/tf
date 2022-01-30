dfbf47fd81ba8e62bbafae9f203396e254ce49d4
mailtogeetha29 <88638201+mailtogeetha29@users.noreply.github.com>
2022-01-29 21:29:07 -0600

    added the required providers

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+#configure the aws provider
 provider "aws" {
   region = var.aws_region
 }
